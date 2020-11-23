d0047aa77dad3d696d71ffd2ef2781aff28f8a6a
wolfi <wolfgang.reder@aon.at>
2020-11-23 19:31:51 +0100

    EDK750 + emergencyMode + isStarted

--- app/edk750/src/main/java/at/or/reder/tools/edk750/EDK750.java
+++ app/edk750/src/main/java/at/or/reder/tools/edk750/EDK750.java
@@ -12,12 +12,17 @@ import at.or.reder.controller.PSControllerEventType;
 import at.or.reder.controller.PSDirection;
 import at.or.reder.dcc.Direction;
 import at.or.reder.dcc.Locomotive;
+import at.or.reder.dcc.LocomotiveFuncEvent;
 import at.or.reder.dcc.PowerMode;
 import at.or.reder.dcc.PowerPort;
 import java.awt.Point;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -37,15 +42,27 @@ public class EDK750
   private int lastWinch;
   private int lastRotate;
   private boolean axisLock = true;
+  private boolean strictMode = true;
+  private long emergencyMode = -1;
+  private long f1On = -1;
+  private final Map<String, String> config;
 
   public EDK750(Locomotive edk,
-                PSController controller)
+                PSController controller,
+                Map<String, String> config)
   {
     this.edk = edk;
+    edk.addLocomotiveFuncEventListener(this::onFunc);
     winch = EDKAxisController.getWinch(edk);
     beam = EDKAxisController.getBeam(edk);
     beamShift = EDKAxisController.getBeamShift(edk);
+    if (config != null) {
+      this.config = Collections.unmodifiableMap(new HashMap<>(config));
+    } else {
+      this.config = Collections.emptyMap();
+    }
     setController(controller);
+    setStarted(edk.isFunction(1));
   }
 
   public boolean isAxisLock()
@@ -56,6 +73,57 @@ public class EDK750
   public void setAxisLock(boolean axisLock)
   {
     this.axisLock = axisLock;
+  }
+
+  public boolean isStrictMode()
+  {
+    return strictMode;
+  }
+
+  public void setStrictMode(boolean strictMode)
+  {
+    this.strictMode = strictMode;
+  }
+
+  public final boolean isStarted()
+  {
+    long f1;
+    synchronized (this) {
+      f1 = f1On;
+    }
+    boolean result = !isStrictMode() || (edk.isFunction(1) && f1 != -1 && (System.currentTimeMillis() - f1) > 2000);
+    LOGGER.log(Level.FINEST,
+               () -> "Engine is " + (result ? "started"
+                                     : "not running"));
+    return result;
+  }
+
+  public final void setStarted(boolean s)
+  {
+    if (isStarted() != s) {
+      try {
+        edk.setFunction(1,
+                        s);
+      } catch (IOException ex) {
+        processException("setStarted",
+                         ex);
+      }
+    }
+  }
+
+  private void onFunc(LocomotiveFuncEvent evt)
+  {
+    if (evt.getFuncNr() == 1) {
+      long f1;
+      if (evt.getFuncValue() != 0) {
+        f1 = System.currentTimeMillis();
+      } else {
+        f1 = -1;
+      }
+      synchronized (this) {
+        f1On = f1;
+      }
+    }
   }
 
   public final PSController getController()
@@ -82,17 +150,49 @@ public class EDK750
     }
   }
 
+  private float getFloatConfig(String key,
+                               float defaultValue)
+  {
+    String tmp = config.getOrDefault(key,
+                                     null);
+    float result = defaultValue;
+    if (tmp != null) {
+      try {
+        result = Float.parseFloat(tmp);
+      } catch (Throwable th) {
+      }
+    }
+    return result;
+  }
+
+  private int getIntConfig(String key,
+                           int defaultValue)
+  {
+    String tmp = config.getOrDefault(key,
+                                     null);
+    int result = defaultValue;
+    if (tmp != null) {
+      try {
+        result = Integer.parseInt(tmp);
+      } catch (Throwable th) {
+      }
+    }
+    return result;
+  }
+
   private void connect()
   {
     if (controller != null) {
       controller.addPropertyChangeListener(controllerListener);
       controllerId = controller.getId();
       controller.getFactory().addPSControllerEventListener(controllerEventListener);
-      controller.setRangeMax(PSController.Axis.RX,
-                             64);
       for (PSController.Axis a : PSController.Axis.values()) {
         controller.setDeadRange(a,
-                                0.10f);
+                                getFloatConfig("dead." + a.name(),
+                                               0.1f));
+        controller.setRangeMax(a,
+                               getIntConfig("max." + a.name(),
+                                            a != PSController.Axis.RX ? 1023 : 64));
       }
     }
   }
@@ -135,13 +235,18 @@ public class EDK750
 
   private void onControllerPropertyChange(PropertyChangeEvent evt)
   {
+    boolean em = isInEmergencyMode();
     try {
       switch (evt.getPropertyName()) {
         case PSController.PROP_LEFT:
-          processLeft(evt);
+          if (!em && isStarted()) {
+            processLeft(evt);
+          }
           break;
         case PSController.PROP_RIGHT:
-          processRight(evt);
+          if (!em && isStarted()) {
+            processRight(evt);
+          }
           break;
         case PSController.PROP_APRESSED:
           processA(evt);
@@ -162,7 +267,9 @@ public class EDK750
           processL1(evt);
           break;
         case PSController.PROP_L2LEVEL:
-          processZAxis(evt);
+          if (!em && isStarted()) {
+            processZAxis(evt);
+          }
           break;
         case PSController.PROP_MODEPRESSED:
           processMode(evt);
@@ -171,7 +278,9 @@ public class EDK750
           processR1(evt);
           break;
         case PSController.PROP_R2LEVEL:
-          processZAxis(evt);
+          if (!em && isStarted()) {
+            processZAxis(evt);
+          }
           break;
         case PSController.PROP_SELECTPRESSED:
           processSelect(evt);
@@ -179,12 +288,79 @@ public class EDK750
         case PSController.PROP_STARTPRESSED:
           processStart(evt);
           break;
+        case PSController.PROP_LEFTPRESSED:
+        case PSController.PROP_RIGHTPRESSED:
+          if (controller.isLeftPressed() && controller.isRightPressed()) {
+            edk.getController().setPowerMode(PowerPort.OUT_1,
+                                             PowerMode.OFF);
+            edk.getController().setPowerMode(PowerPort.OUT_2,
+                                             PowerMode.OFF);
+            setInEmergencyMode(true,
+                               true);
+          } else if (controller.isLeftPressed() & !em) {
+            edk.getController().setPowerMode(PowerPort.OUT_1,
+                                             PowerMode.SSPEM);
+            edk.getController().setPowerMode(PowerPort.OUT_2,
+                                             PowerMode.SSPEM);
+            setInEmergencyMode(true,
+                               true);
+          } else if (controller.isRightPressed() && !em) {
+            edk.setFunction(3,
+                            false);
+            edk.setFunction(4,
+                            false);
+            edk.setFunction(5,
+                            false);
+            edk.setFunction(6,
+                            false);
+            edk.setFunction(7,
+                            false);
+            edk.setFunction(8,
+                            false);
+            edk.setFunction(18,
+                            false);
+            edk.setFunction(19,
+                            false);
+            edk.control(Direction.FORWARD,
+                        0);
+            edk.setFunction(2,
+                            false);
+            setInEmergencyMode(true,
+                               false);
+          }
+          break;
         default:
       }
     } catch (IOException ex) {
       processException(evt.getPropertyName(),
                        ex);
     }
+  }
+
+  private void setInEmergencyMode(boolean en,
+                                  boolean stopEngine)
+  {
+    if (en) {
+      emergencyMode = System.currentTimeMillis();
+      if (stopEngine) {
+        setStarted(false);
+      }
+    } else {
+      emergencyMode = -1;
+    }
+    LOGGER.log(Level.FINER,
+               () -> "Setting emergencymode to " + (en ? "on" : "off"));
+  }
+
+  private boolean isInEmergencyMode()
+  {
+    long delta = (System.currentTimeMillis() - emergencyMode);
+    boolean result = emergencyMode != -1 && delta < 5000;
+    LOGGER.log(Level.FINEST,
+               () -> MessageFormat.format("Emergency mode is {0} ({1} ms))",
+                                          new Object[]{result ? "on" : "off",
+                                                       result ? delta : -1}));
+    return result;
   }
 
   private Point applyAxisLock(Point p)
@@ -261,15 +437,16 @@ public class EDK750
     if (dir.isNorth()) {
       edk.toggleFunction(16);
     }
-    if (dir.isSouth()) {
-      edk.toggleFunction(17);
+    if (!dir.isWest() && !dir.isEast()) {
+      edk.setFunction(17,
+                      dir.isSouth());
     }
-    if (dir.isWest()) {
+    if (dir.isWest() && !isInEmergencyMode()) {
       edk.setFunction(19,
                       false);
       edk.toggleFunction(18);
     }
-    if (dir.isEast()) {
+    if (dir.isEast() && !isInEmergencyMode()) {
       edk.setFunction(18,
                       false);
       edk.toggleFunction(19);
@@ -316,8 +493,14 @@ public class EDK750
 
   protected void processSelect(PropertyChangeEvent evt) throws IOException
   {
-    edk.getController().setPowerMode(PowerPort.OUT_1,
-                                     PowerMode.ON);
+    if (controller.isSelectPressed()) {
+      edk.getController().setPowerMode(PowerPort.OUT_1,
+                                       PowerMode.ON);
+      edk.getController().setPowerMode(PowerPort.OUT_2,
+                                       PowerMode.ON);
+      setInEmergencyMode(false,
+                         true);
+    }
   }
 
   protected void processStart(PropertyChangeEvent evt) throws IOException
